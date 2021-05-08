642ee1636c397fbacdac5b61eefde134c96355c8
Lucas Pinheiro <lucaspinheiru@hotmail.com>
2021-05-08 01:16:45 +0000

    Recreate seeds and schemas

---
+++ backend/db/seeds/01_users.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (name, email, password) VALUES ('Lucas Pinheiro', 'lucas@hotmail.com', 'test');
+INSERT INTO users (name, email, password) VALUES ('Gabriel Silva', 'gabriel@hotmail.com', 'test');
+INSERT INTO users (name, email, password) VALUES ('Lucas Pinheiro', 'omar@hotmail.com', 'test');
---
+++ backend/db/seeds/05_budgets.sql
@@ -0,0 +1,2 @@
+INSERT INTO budgets (user_id, account_id, type, budget_amount, budget_cycle, cycle_start) VALUES (1, 1, 'Fixed', 150000, 'Monthly', '2021-06-01');
+
