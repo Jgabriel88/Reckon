7ff572a2a601fb082083b2f074ecb5bc9cf5ff77
Gabriel Silva <j.gabriel.888@gmail.com>
2021-05-08 19:37:22 +0000

    fix configuration files

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,39 @@
-import AccountsPanelList from "./components/AccountsPanelList";
-import BillPanelList from "./components/BillPanelList";
-import MontlyTarget from "./components/MontlyTarget";
-import SideNavList from "./components/SideNavList";
-import TopNav from "./components/TopNav";
-import GraphList from "./components/GraphList";
-import "./App.scss";
+import { useEffect } from 'react';
+import AccountsPanelList from './components/AccountsPanelList';
+import BillPanelList from './components/BillPanelList';
+import MontlyTarget from './components/MontlyTarget';
+import SideNavList from './components/SideNavList';
+import TopNav from './components/TopNav';
+import GraphList from './components/GraphList';
+import './App.scss';
+import axios from 'axios';
 
 function App() {
-  function App() {
-    useEffect(() => {
-      axios.get(`http://localhost:3000/accounts`).then((res) => {
-        console.log("WE ARE PRINTING HERE ---------->", res);
-      });
-    });
+	useEffect(() => {
+		axios.get(`http://localhost:3000/api/accounts`).then((res) => {
+			console.log('WE ARE PRINTING HERE ---------->', res);
+		});
+	});
 
-    return (
-      <main className="layout">
-        <section className="sidebar">
-          <nav className="sidebar__menu">
-            <SideNavList />
-          </nav>
-        </section>
+	return (
+		<main className="layout">
+			<section className="sidebar">
+				<nav className="sidebar__menu">
+					<SideNavList />
+				</nav>
+			</section>
 
-        <section className="content">
-          <section className="top_nav">
-            <TopNav />
-          </section>
-          <GraphList />
-          <AccountsPanelList />
-          <MontlyTarget />
-          <BillPanelList />
-        </section>
-      </main>
-    );
-  }
+			<section className="content">
+				<section className="top_nav">
+					<TopNav />
+				</section>
+				<GraphList />
+				<AccountsPanelList />
+				<MontlyTarget />
+				<BillPanelList />
+			</section>
+		</main>
+	);
 }
+
 export default App;
--- client/src/components/SideNavList.jsx
+++ client/src/components/SideNavList.jsx
@@ -1,29 +1,10 @@
-<<<<<<< HEAD
-import SideNavItem from "./SideNavItem";
-import "./SideNavList.css";
-
-const SideNavList = () => {
-  return (
-    <div className="sidenav">
-      <nav>
-        <SideNavItem name="Dashboard" />
-        <SideNavItem name="Income" />
-        <SideNavItem name="Expenses" />
-        <SideNavItem name="Accounts" />
-        <SideNavItem name="Budgets" />
-        <SideNavItem name="Reports" />
-      </nav>
-    </div>
-  );
-=======
 import SideNavItem from './SideNavItem';
 import Logo from './Logo';
-import "./SideNavList.scss";
+import './SideNavList.scss';
 
 const SideNavList = () => {
 	return (
 		<div className="side_nav">
-			
 			<section>
 				<Logo />
 				<SideNavItem name="Dashboard" />
@@ -35,7 +16,6 @@ const SideNavList = () => {
 			</section>
 		</div>
 	);
->>>>>>> master
 };
 
 export default SideNavList;
