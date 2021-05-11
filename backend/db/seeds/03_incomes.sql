eec60b117d43f86953a8b2b69890f5c71da72ed2
vagrant <sribas@gmail.com>
2021-05-11 09:20:04 +0000

    add seed to income for 5 months ago

--- backend/db/seeds/03_incomes.sql
+++ backend/db/seeds/03_incomes.sql
@@ -1,3 +1,6 @@
+INSERT INTO incomes (user_id, account_id, description, date, amount, notes) VALUES (1, 1, 'Daily Sales', now() - interval '5 month', 45000, 'Daily Sales for the 11th');
+INSERT INTO incomes (user_id, account_id, description, date, amount, notes) VALUES (1, 1, 'Daily Sales', now() - interval '5 month', 50000, 'Daily Sales for the 11th');
+INSERT INTO incomes (user_id, account_id, description, date, amount, notes) VALUES (1, 1, 'Daily Sales', now() - interval '5 month', 80000, 'Daily Sales for the 11th');
 INSERT INTO incomes (user_id, account_id, description, date, amount, notes) VALUES (1, 1, 'Daily Sales', now() - interval '4 month', 35000, 'Daily Sales for the 11th');
 INSERT INTO incomes (user_id, account_id, description, date, amount, notes) VALUES (1, 1, 'Daily Sales', now() - interval '4 month', 55000, 'Daily Sales for the 11th');
 INSERT INTO incomes (user_id, account_id, description, date, amount, notes) VALUES (1, 1, 'Daily Sales', now() - interval '4 month', 45000, 'Daily Sales for the 11th');
