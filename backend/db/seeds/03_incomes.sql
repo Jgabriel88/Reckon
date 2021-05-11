622203554c75ed8f05888489308368c3379413cc
vagrant <sribas@gmail.com>
2021-05-11 19:18:48 +0000

    fix column name on incomes.sql

--- backend/db/seeds/03_incomes.sql
+++ backend/db/seeds/03_incomes.sql
@@ -1,5 +1,5 @@
 INSERT INTO incomes 
-(user_id, account_id, description, date, amount, notes) 
+(user_id, account_id, description, date, amount_cents, notes) 
 VALUES 
 (1, 1, 'Daily Sales', now() - interval '5 month', 45000, 'Daily Sales for the 11th'),
 (1, 1, 'Daily Sales', now() - interval '5 month', 50000, 'Daily Sales for the 11th'),
@@ -17,7 +17,7 @@ VALUES
 (1, 1, 'Daily Sales', now() - interval '1 month', 35000, 'Daily Sales for the 11th'),
 (1, 1, 'Daily Sales', now() - interval '1 month', 47000, 'Daily Sales for the 11th');
 INSERT INTO incomes 
-(user_id, account_id, description, amount, notes) 
+(user_id, account_id, description, amount_cents, notes) 
 VALUES 
 (1, 1, 'Daily Sales', 50000, 'Daily Sales for the 5th'),
 (1, 1, 'Daily Sales', 40000, 'Daily Sales for the 5th'),
