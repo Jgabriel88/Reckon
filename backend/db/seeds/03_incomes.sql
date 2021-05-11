299af52efaf43e4979f19681acf1ec9bfba7ad3b
vagrant <sribas@gmail.com>
2021-05-11 09:17:48 +0000

    add seed to income for 2 months ago

--- backend/db/seeds/03_incomes.sql
+++ backend/db/seeds/03_incomes.sql
@@ -1,3 +1,9 @@
+INSERT INTO incomes (user_id, account_id, description, date, amount, notes) VALUES (1, 1, 'Daily Sales', now() - interval '2 month', 30000, 'Daily Sales for the 11th');
+INSERT INTO incomes (user_id, account_id, description, date, amount, notes) VALUES (1, 1, 'Daily Sales', now() - interval '2 month', 65000, 'Daily Sales for the 11th');
+INSERT INTO incomes (user_id, account_id, description, date, amount, notes) VALUES (1, 1, 'Daily Sales', now() - interval '2 month', 60000, 'Daily Sales for the 11th');
+INSERT INTO incomes (user_id, account_id, description, date, amount, notes) VALUES (1, 1, 'Daily Sales', now() - interval '1 month', 70000, 'Daily Sales for the 11th');
+INSERT INTO incomes (user_id, account_id, description, date, amount, notes) VALUES (1, 1, 'Daily Sales', now() - interval '1 month', 35000, 'Daily Sales for the 11th');
+INSERT INTO incomes (user_id, account_id, description, date, amount, notes) VALUES (1, 1, 'Daily Sales', now() - interval '1 month', 47000, 'Daily Sales for the 11th');
 INSERT INTO incomes (user_id, account_id, description, amount, notes) VALUES (1, 1, 'Daily Sales', 50000, 'Daily Sales for the 5th');
 INSERT INTO incomes (user_id, account_id, description, amount, notes) VALUES (1, 1, 'Daily Sales', 40000, 'Daily Sales for the 5th');
 INSERT INTO incomes (user_id, account_id, description, amount, notes) VALUES (1, 1, 'Daily Sales', 35000, 'Daily Sales for the 5th'); 
