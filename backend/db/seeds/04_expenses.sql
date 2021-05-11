23eac1fa0c3caf545d08ae688c875013f3cf5285
vagrant <sribas@gmail.com>
2021-05-11 10:57:52 +0000

    add expenses items to expenses.sql seed file

--- backend/db/seeds/04_expenses.sql
+++ backend/db/seeds/04_expenses.sql
@@ -1,3 +1,6 @@
+INSERT INTO expenses (user_id, account_id, category, sub_category, date, amount_cents, payee, notes) VALUES (1, 1, 'Supplies', 'Packaging', now() - interval '1 month', 10000, 'Staples', 'Packaging supply run for the week');
+INSERT INTO expenses (user_id, account_id, category, sub_category, date, amount_cents, payee, notes) VALUES (1, 1, 'Supplies', 'POS', now() - interval '1 month', 17000, 'Staples', 'Receipt rolls');
+INSERT INTO expenses (user_id, account_id, category, sub_category, date, amount_cents, payee, notes) VALUES (1, 1, 'Supplies', 'Payroll', now() - interval '1 month', 210000, 'Payroll', 'Payroll for the week');
 INSERT INTO expenses (user_id, account_id, category, sub_category, amount_cents, payee, notes) VALUES (1, 1, 'Supplies', 'Packaging', 15000, 'Staples', 'Packaging supply run for the week');
 INSERT INTO expenses (user_id, account_id, category, sub_category, amount_cents, payee, notes) VALUES (1, 1, 'Supplies', 'POS', 15000, 'Staples', 'Receipt rolls');
 INSERT INTO expenses (user_id, account_id, category, sub_category, amount_cents, payee, notes) VALUES (1, 1, 'Supplies', 'Payroll', 200000, 'Payroll', 'Payroll for the week');
