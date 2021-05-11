4be2105f47a8c8d8f278eec88a72b4f6087357bd
vagrant <sribas@gmail.com>
2021-05-11 19:37:46 +0000

    rename props.amount to props.amount_cents on src/components/Income/IncomeSummaryItem.jsx

--- client/src/components/Income/IncomeSummaryItem.jsx
+++ client/src/components/Income/IncomeSummaryItem.jsx
@@ -7,7 +7,7 @@ const IncomeSummaryItem = (props) => {
 			<td>{props.id}</td>
 			<td>{new Date(props.date).toISOString().split('T')[0]}</td>
 			<td>{props.description}</td>
-			<td>${props.amount}</td>
+			<td>${props.amount_cents}</td>
 			<td>
 				<FaIcons.FaEdit className="income_list_edit" /> |{' '}
 				<FaIcons.FaTrashAlt className="income_list_delete" />
