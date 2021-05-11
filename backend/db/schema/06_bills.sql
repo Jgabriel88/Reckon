5b8af75278a4313c8fd5aec07ff64a3d4b855d23
vagrant <sribas@gmail.com>
2021-05-11 19:17:49 +0000

    rename amount to amount_cents on bills table

--- backend/db/schema/06_bills.sql
+++ backend/db/schema/06_bills.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS bills CASCADE;
 CREATE TABLE bills (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  amount INTEGER NOT NULL,
+  amount_cents INTEGER NOT NULL,
   payee VARCHAR(255),
   due_date DATE,
   is_paid BOOLEAN DEFAULT false
