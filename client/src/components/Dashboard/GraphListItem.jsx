5e4d47c037659a9eb42b912ff158278385bb57bc
vagrant <sribas@gmail.com>
2021-05-11 18:31:39 +0000

    add hooks to draw the graphs

--- client/src/components/Dashboard/GraphListItem.jsx
+++ client/src/components/Dashboard/GraphListItem.jsx
@@ -1,11 +1,28 @@
+import { Chart } from 'react-charts';
+import useGraph from '../../hooks/useGraph';
+import { useEffect } from 'react';
 
 const GraphListItem = (props) => {
 
+
+	useEffect(()=>{
+		window.dispatchEvent(new Event('resize'));
+	}, [])
+
+	const { data, series, axes } = useGraph(props.data);
+
+	// A react-chart hyper-responsively and continuously fills the available
+	// space of its parent element automatically
 	return (
-		<div>
-			{props.month} ---- {props.sum}
-		</div>
-	);
-};
+	<div
+		style={{
+			width: 'auto',
+			height: '100%'
+		}}
+	>
+		<Chart data={data} series={series} axes={axes} />
+	</div>
+	)
+}
 
 export default GraphListItem;
