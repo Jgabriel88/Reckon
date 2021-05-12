8adfdae0cb6a491cfe85f5b29e1552a9a57dd572
Omar Cabatbat <omar_cabatbat@yahoo.com>
2021-05-12 14:57:46 +0000

    Added functionality to show/hide the account details

--- client/src/components/Accounts/index.jsx
+++ client/src/components/Accounts/index.jsx
@@ -1,9 +1,13 @@
+import {useState} from "react";
 import { Link } from 'react-router-dom';
-import {Container} from "react-bootstrap";
+import {Container, Table} from "react-bootstrap";
 import * as FaIcons from 'react-icons/fa';
 import './Accounts.scss';
 
 const Accounts = () => {
+  const [accountDetails, setAccountDetails] = useState(false);
+  const showDetails = () => setAccountDetails(!accountDetails);
+  
   return (
     <Container className="account_summary">
       <section className="account_summary_bank">
@@ -25,8 +29,40 @@ const Accounts = () => {
             <p>Total: $ 1,000.00</p>
           </div>
           <div>
-            <button class="account_summary_details_btn_show"><FaIcons.FaEllipsisV /></button>
+            <button class="account_summary_details_btn_show" onClick={showDetails}><FaIcons.FaEllipsisV /></button>
           </div>
+        </div>
+        <div className={accountDetails ? 'account_summary_details_show active' : 'account_summary_details_show'}>
+          <Table striped hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Date</th>
+                <th>Activity</th>
+                <th>Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>02/28/2021</td>
+                <td>Deposit</td>
+                <td>$ 500.00</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>02/28/2021</td>
+                <td>Deposit</td>
+                <td>$ 500.00</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>02/28/2021</td>
+                <td>Withdrawal</td>
+                <td>$ 500.00</td>
+              </tr>
+            </tbody>
+          </Table>
         </div>
         <div className="account_summary_details">
           <div>
@@ -37,8 +73,40 @@ const Accounts = () => {
             <p>Total: $ 1,198.00</p>
           </div>
           <div>
-            <button class="account_summary_details_btn_show"><FaIcons.FaEllipsisV /></button>
+            <button class="account_summary_details_btn_show" onClick={showDetails}><FaIcons.FaEllipsisV /></button>
           </div>
+        </div>
+        <div className={accountDetails ? 'account_summary_details_show active' : 'account_summary_details_show'}>
+          <Table striped hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Date</th>
+                <th>Activity</th>
+                <th>Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>02/28/2021</td>
+                <td>Deposit</td>
+                <td>$ 500.00</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>02/28/2021</td>
+                <td>Deposit</td>
+                <td>$ 500.00</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>02/28/2021</td>
+                <td>Withdrawal</td>
+                <td>$ 500.00</td>
+              </tr>
+            </tbody>
+          </Table>
         </div>
 			</section>
       <section className="account_summary_ccard">
