51869c5a1a8c344e023b4763bb17e9d6c0d57581
Gabriel Silva <j.gabriel.888@gmail.com>
2021-05-12 11:40:50 -0400

    new expense form route

--- backend/db/seeds/04_expenses.sql
+++ backend/db/seeds/04_expenses.sql
@@ -1,24 +1,18 @@
 INSERT INTO expenses 
-(user_id, account_id, category, sub_category, date, amount_cents, payee, notes) 
+(user_id, account_id, category, date, amount_cents, payee, notes) 
 VALUES 
-(1, 1, 'Supplies', 'Packaging', now() - interval '5 month', 2900000, 'Staples', 'Packaging supply run for the week'),
-(1, 1, 'Supplies', 'POS', now() - interval '5 month', 1500000, 'Staples', 'Receipt rolls'),
-(1, 1, 'Supplies', 'Payroll', now() - interval '5 month', 50450000, 'Payroll', 'Payroll for the week'),
-(1, 1, 'Supplies', 'Packaging', now() - interval '4 month', 1800000, 'Staples', 'Packaging supply run for the week'),
-(1, 1, 'Supplies', 'POS', now() - interval '4 month', 2100000, 'Staples', 'Receipt rolls'),
-(1, 1, 'Supplies', 'Payroll', now() - interval '4 month', 20450000, 'Payroll', 'Payroll for the week'),
-(1, 1, 'Supplies', 'Packaging', now() - interval '3 month', 2400000, 'Staples', 'Packaging supply run for the week'),
-(1, 1, 'Supplies', 'POS', now() - interval '3 month', 1920000, 'Staples', 'Receipt rolls'),
-(1, 1, 'Supplies', 'Payroll', now() - interval '3 month', 21450000, 'Payroll', 'Payroll for the week'),
-(1, 1, 'Supplies', 'Packaging', now() - interval '2 month', 2000000, 'Staples', 'Packaging supply run for the week'),
-(1, 1, 'Supplies', 'POS', now() - interval '2 month', 1800000, 'Staples', 'Receipt rolls'),
-(1, 1, 'Supplies', 'Payroll', now() - interval '2 month', 21000000, 'Payroll', 'Payroll for the week'),
-(1, 1, 'Supplies', 'Packaging', now() - interval '1 month', 1000000, 'Staples', 'Packaging supply run for the week'),
-(1, 1, 'Supplies', 'POS', now() - interval '1 month', 1700000, 'Staples', 'Receipt rolls'),
-(1, 1, 'Supplies', 'Payroll', now() - interval '1 month', 21000000, 'Payroll', 'Payroll for the week');
-INSERT INTO expenses 
-(user_id, account_id, category, sub_category, amount_cents, payee, notes) 
-VALUES 
-(1, 1, 'Supplies', 'Packaging', 1500000, 'Staples', 'Packaging supply run for the week'),
-(1, 1, 'Supplies', 'POS', 1500000, 'Staples', 'Receipt rolls'),
-(1, 1, 'Supplies', 'Payroll', 20000000, 'Payroll', 'Payroll for the week');
+(1, 1, 'Bank Fees', now() - interval '5 month', 2900000, 'Staples', 'Bank Fees for the week'),
+(1, 1, 'Supplies', now() - interval '5 month', 1500000, 'Staples', 'Receipt rolls'),
+(1, 1, 'Payroll', now() - interval '5 month', 50450000, 'Payroll', 'Payroll for the week'),
+(1, 1, 'Insurance', now() - interval '4 month', 1800000, 'Staples', 'Monthly Insurance'),
+(1, 1, 'Supplies', now() - interval '4 month', 2100000, 'Staples', 'Receipt rolls'),
+(1, 1, 'Payroll', now() - interval '4 month', 20450000, 'Payroll', 'Payroll for the week'),
+(1, 1, 'Supplies', now() - interval '3 month', 2400000, 'Staples', 'Packaging supply run for the week'),
+(1, 1, 'Utilities', now() - interval '3 month', 1920000, 'Staples', 'Monthly Hydro Bill'),
+(1, 1, 'Payroll', now() - interval '3 month', 21450000, 'Payroll', 'Payroll for the week'),
+(1, 1, 'Supplies', now() - interval '2 month', 2000000, 'Staples', 'Packaging supply run for the week'),
+(1, 1, 'Meals', now() - interval '2 month', 1800000, 'Staples', 'Money spent in meals for the month'),
+(1, 1, 'Payroll', now() - interval '2 month', 21000000, 'Payroll', 'Payroll for the week'),
+(1, 1, 'Supplies', now() - interval '1 month', 1000000, 'Staples', 'Packaging supply run for the week'),
+(1, 1, 'Supplies', now() - interval '1 month', 1700000, 'Staples', 'Receipt rolls'),
+(1, 1, 'Payroll', now() - interval '1 month', 21000000, 'Payroll', 'Payroll for the week');
