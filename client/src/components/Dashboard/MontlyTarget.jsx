d8863bad0c4074bbeedce696c860b057e46d5ee1
Omar Cabatbat <omar_cabatbat@yahoo.com>
2021-05-13 13:28:09 +0000

    Fix Monthly Target position

--- client/src/components/Dashboard/MontlyTarget.jsx
+++ client/src/components/Dashboard/MontlyTarget.jsx
@@ -1,10 +1,10 @@
 import "./MonthlyTarget.scss";
 
-const MontlyTarget = () => {
+const MonthlyTarget = () => {
 	return (
 			<div className="target">
 				<h1>$ 7,000.00</h1>
 			</div>
 		)};
 
-export default MontlyTarget;
+export default MonthlyTarget;
