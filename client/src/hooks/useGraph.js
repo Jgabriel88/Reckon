ffc048237d5414a416d4f34fdfe3e065003c2172
vagrant <sribas@gmail.com>
2021-05-13 04:52:44 +0000

    remove import orderByMonth function from useGraph hook

--- client/src/hooks/useGraph.js
+++ client/src/hooks/useGraph.js
@@ -1,5 +1,4 @@
 import { useMemo } from 'react';
-import { orderByMonth } from '../helpers/orderByMonth';
 
 const months = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
