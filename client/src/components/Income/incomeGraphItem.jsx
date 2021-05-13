08a9dc989b72c77e1fb29f16bc8ef9e5dc31f697
vagrant <sribas@gmail.com>
2021-05-13 01:06:37 +0000

    add incomeGraphItem component

---
+++ client/src/components/Income/incomeGraphItem.jsx
@@ -0,0 +1,28 @@
+import { Chart } from 'react-charts';
+import useGraph from '../../hooks/useGraph';
+import { useEffect } from 'react';
+
+const incomeGraphItem = (props) => {
+
+	useEffect(()=>{
+		window.dispatchEvent(new Event('resize'));
+	}, [])
+
+	const { data, series, axes } = useGraph(props.data);
+
+	// A react-chart hyper-responsively and continuously fills the available
+	// space of its parent element automatically
+	return (
+	<div
+		style={{
+			width: 'auto',
+			height: '100%'
+		}}
+	>
+		<Chart data={data} series={series} axes={axes} tooltip/>
+
+	</div>
+	)
+}
+
+export default incomeGraphItem;
