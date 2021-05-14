aaddb354fb352d17facfa5dfbd9f5eee931edbf2
Omar Cabatbat <omar_cabatbat@yahoo.com>
2021-05-14 00:10:01 +0000

    Fix Broken layout

--- client/src/components/users/login.jsx
+++ client/src/components/users/login.jsx
@@ -3,23 +3,23 @@ import * as FaIcons from 'react-icons/fa';
 
 const login = () => {
   return (
-    <div className="container">
-      <div className="container_fold">
-        <div className="container_wrapper">
+    <div className="user_container">
+      <div className="user_container_fold">
+        <div className="user_container_wrapper">
           <img src='images/n_logo.png' className="img_logo" alt="logo"/>
           <h3>LOG IN</h3>
           <div>
             <form>
-              <div className="input-container">
+              <div className="user_input-container">
                 <label for="email">Email</label>
-                <input type="email" placeholder='abc@abc.com' className="form_input" name="email" />
+                <input type="email" placeholder='abc@abc.com' className="user_form_input" name="email" />
               </div>
-              <div className="input-container">
+              <div className="user_input-container">
                 <label for="email">Password</label>
-                <input type="password" placeholder="password" className="form_input" name="password" />
+                <input type="password" placeholder="password" className="user_form_input" name="password" />
               </div>
-              <div className="input-container">
-                <button type="submit" className="input-container_btn-login">Log In</button>
+              <div className="user_input-container">
+                <button type="submit" className="user_input-container_btn-login">Log In</button>
               </div>
             </form>
           </div>
--- client/src/components/users/register.jsx
+++ client/src/components/users/register.jsx
@@ -2,31 +2,31 @@ import './Users.scss';
 
 const register = () => {
   return (
-    <div className="container">
-      <div className="container_fold">
-        <div className="container_wrapper">
+    <div className="user_container">
+      <div className="user_container_fold">
+        <div className="user_container_wrapper">
           <img src='images/n_logo.png' className="img_logo" alt="logo"/>
           <h3>USER REGISTRATION</h3>
           <div>
             <form>
-              <div className="input-container">
+              <div className="user_input-container">
                 <label for="name">Name</label>
-                <input type="text" placeholder='Enter Full Name' className="form_input" name="name" />
+                <input type="text" placeholder='Enter Full Name' className="user_form_input" name="name" />
               </div>
-              <div className="input-container">
+              <div className="user_input-container">
                 <label for="email">Email</label>
-                <input type="email" placeholder='abc@abc.com' className="form_input" name="email" />
+                <input type="email" placeholder='abc@abc.com' className="user_form_input" name="email" />
               </div>
-              <div className="input-container">
+              <div className="user_input-container">
                 <label for="password">Password</label>
-                <input type="password" placeholder="password" className="form_input" name="password"/>
+                <input type="password" placeholder="password" className="user_form_input" name="password"/>
               </div>
-              <div className="input-container">
+              <div className="user_input-container">
                 <label for="password">Confirm Password</label>
-                <input type="password" placeholder="confirm password" className="form_input" name="confirm_password"/>
+                <input type="password" placeholder="confirm password" className="user_form_input" name="confirm_password"/>
               </div>
-              <div className="input-container">
-                <button type="submit" className="input-container_btn-login">Register</button>
+              <div className="user_input-container">
+                <button type="submit" className="user_input-container_btn-login">Register</button>
               </div>
             </form>
           </div>
