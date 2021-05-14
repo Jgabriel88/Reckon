b41a33e0264987dd16d6ff7e4c6b69f09d2dd24c
vagrant <sribas@gmail.com>
2021-05-14 19:42:58 +0000

    add report router and report queries

---
+++ backend/db/report-queries.js
@@ -0,0 +1,81 @@
+const { response } = require('express');
+const db = require('./db');
+
+const getIncomes = () => {
+	return db
+		.query(
+			'SELECT * FROM incomes, (SELECT SUM(amount_cents) FROM incomes) AS total;'
+		)
+		.then((response) => {
+			return response.rows;
+		});
+};
+
+const grabAccountId = (srt) => {
+	return srt.split(' ')[0];
+};
+
+const deleteIncome = (id) => {
+	return db.query('DELETE FROM incomes WHERE id = $1;', [id]).then((res) => {
+		return res.rows;
+	});
+};
+
+const editIncome = (newData, id) => {
+	let newId = parseInt(grabAccountId(newData.account));
+	return db.query(
+		`UPDATE incomes
+	 SET account_id=$1,
+	 description=$2,
+	 amount_cents=$3,
+   notes=$4
+	 WHERE id=$5`,
+		[newId, newData.description, newData.amount, newData.notes, Number(id)]
+	);
+};
+
+const addIncome = (newData) => {
+	let id = parseInt(grabAccountId(newData.account));
+	let date = new Date();
+	return db.query(
+		'INSERT INTO incomes (user_id, account_id, description, date, amount_cents, notes) VALUES ($1,$2,$3,$4,$5,$6)',
+		[1, id, newData.description, date, newData.amount, newData.notes]
+	);
+};
+
+const getIncomeById = (id) => {
+	return db
+		.query('SELECT * FROM incomes WHERE id = $1', [id])
+		.then((response) => {
+			return response.rows[0];
+		});
+};
+
+// Return Income Report Data
+const getIncomesDateInterval = (startDate, endDate) => {
+		
+	// Return Total Income (Gross Sales) 
+	sqlQuery =  'SELECT SUM(amount_cents) ';
+	sqlQuery += 'FROM incomes ';
+	sqlQuery += 'WHERE date BETWEEN $1 AND $2;';
+
+	return db
+		.query(sqlQuery, [startDate, endDate])
+		.then((response) => {
+			return response.rows
+		});
+
+
+
+
+};
+
+module.exports = {
+	getIncomes,
+	getIncomeById,
+	getMonthlyIncomes,
+	getIncomesDateInterval,
+	addIncome,
+	deleteIncome,
+	editIncome,
+};
---
+++ backend/routes/report-router.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const {
+	getReportDateInterval,
+} = require('../db/report-queries');
+
+// GET /income/:startdate/:enddate
+router.get('/:startdate/:enddate', (req, res) => {
+	getIncomeReportDateInterval(req.params.startdate, req.params.enddate).then((incomeReportDateInterval) => {
+		res.json(incomeReportDateInterval);
+	});
+});
+
+module.exports = router;
