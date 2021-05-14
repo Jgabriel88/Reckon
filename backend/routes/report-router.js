e68d9dcc1de06adb99ef042ebdf60cb1c73e8709
vagrant <sribas@gmail.com>
2021-05-14 19:55:37 +0000

    add functions to report-queries.js

--- backend/routes/report-router.js
+++ backend/routes/report-router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {
-	getReportDateInterval,
+	getIncomeReportDateInterval,
 } = require('../db/report-queries');
 
 // GET /income/:startdate/:enddate
