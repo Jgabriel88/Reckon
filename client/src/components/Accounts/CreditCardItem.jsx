676df68a7e0460b94ffb9825bafd56a9cde69243
Gabriel Silva <j.gabriel.888@gmail.com>
2021-05-17 10:32:32 -0400

    fix conflicts

--- client/src/components/Accounts/CreditCardItem.jsx
+++ client/src/components/Accounts/CreditCardItem.jsx
@@ -5,7 +5,6 @@ import * as FaIcons from 'react-icons/fa';
 import './Accounts.scss';
 
 const CreditCardsList = () => {
-<<<<<<< HEAD
 	const [accountDetails, setAccountDetails] = useState(false);
 	const showDetails = () => setAccountDetails(!accountDetails);
 
@@ -17,7 +16,7 @@ const CreditCardsList = () => {
 					<p>No Limit 06301-123455</p>
 				</div>
 				<div className="align-center">
-					<p>Total:$1,798.00</p>
+					<p>Total: $ 1,798.00</p>
 				</div>
 				<div>
 					<button
@@ -36,7 +35,6 @@ const CreditCardsList = () => {
 				<Table striped hover>
 					<thead>
 						<tr>
-							<th>#</th>
 							<th>Date</th>
 							<th>Details</th>
 							<th>Amount</th>
@@ -44,19 +42,16 @@ const CreditCardsList = () => {
 					</thead>
 					<tbody>
 						<tr>
-							<td>1</td>
 							<td>02/28/2021</td>
 							<td>Uber Eats</td>
 							<td>$ 20.00</td>
 						</tr>
 						<tr>
-							<td>1</td>
 							<td>02/28/2021</td>
 							<td>Payment</td>
 							<td>$ -500.00</td>
 						</tr>
 						<tr>
-							<td>1</td>
 							<td>02/28/2021</td>
 							<td>Best Buy</td>
 							<td>$ 1,500.00</td>
@@ -68,54 +63,3 @@ const CreditCardsList = () => {
 	);
 };
 export default CreditCardsList;
-=======
-  const [accountDetails, setAccountDetails] = useState(false);
-  const showDetails = () => setAccountDetails(!accountDetails);
-  
-  return (
-    <>
-      <div className="account_summary_details">
-        <div>
-          <h5>RBC Rewards</h5>
-          <p>No Limit 06301-123455</p>
-        </div>
-        <div className="align-center">
-          <p>Total: $ 1,798.00</p>
-        </div>
-        <div>
-          <button class="account_summary_details_btn_show" onClick={showDetails}><FaIcons.FaEllipsisV /></button>
-        </div>
-      </div>
-      <div className={accountDetails ? 'account_summary_details_show active' : 'account_summary_details_show'}>
-        <Table striped hover>
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Details</th>
-              <th>Amount</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>02/28/2021</td>
-              <td>Uber Eats</td>
-              <td>$ 20.00</td>
-            </tr>
-            <tr>
-              <td>02/28/2021</td>
-              <td>Payment</td>
-              <td>$ -500.00</td>
-            </tr>
-            <tr>
-              <td>02/28/2021</td>
-              <td>Best Buy</td>
-              <td>$ 1,500.00</td>
-            </tr>
-          </tbody>
-        </Table>
-      </div>
-    </>
-  )
-}
-export default CreditCardsList;
->>>>>>> master
